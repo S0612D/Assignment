ae77a4b8303804cd47f30eb32d9ae1de4008f466
S0612D <shubhamdornal612@gmail.com>
2023-10-25 13:17:02 +0530

    second commit

---
+++ js/custom.js
@@ -0,0 +1,255 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+// isotope js
+$(window).on('load', function () {
+    $('.filters_menu li').click(function () {
+        $('.filters_menu li').removeClass('active');
+        $(this).addClass('active');
+
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: data
+        })
+    });
+
+    var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: false,
+        masonry: {
+            columnWidth: ".all"
+        }
+    })
+});
+
+// nice select
+$(document).ready(function() {
+    $('select').niceSelect();
+  });
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        300: {
+            items: 2
+        },
+        900: {
+            items: 2
+        }
+    }
+});
+
+// Function to handle form submission
+function submit() {
+    // Display alert message on successful form submission
+    alert('Form Submitted Successfully');
+}
+
+//form validation
+function validate(){
+    var form = document.getElementById("form")
+    var name = document.getElementById("name");
+    var num = document.getElementById("num");
+    var email = document.getElementById("email");
+    var select = document.getElementById("select");
+    var date = document.getElementById("date-input");
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        
+        checkInputs();
+    });
+    
+    function checkInputs() {
+        // trim to remove the whitespaces
+        const nameValue = name.value.trim();
+        const emailValue = email.value.trim();
+        const numValue = num.value.trim();
+        const selectValue = select.value;
+        const dateValue = date.value()
+        
+        if(nameValue === '') {
+            setErrorFor(name, 'Username cannot be blank');
+        } else {
+            setSuccessFor(name);
+        }
+        
+        if(emailValue === '') {
+            setErrorFor(email, 'Email cannot be blank');
+        } else if (!email(emailValue)) {
+            setErrorFor(email, 'Not a valid email');
+        } else {
+            setSuccessFor(email);
+        }
+        
+        if(numValue === '') {
+            setErrorFor(num, 'Phone Number cannot be blank');
+        } else {
+            setSuccessFor(num);
+        }
+        
+        if(selectValue === '') {
+            setErrorFor(select, 'Persons cannot be blank');
+        } else {
+            setSuccessFor(select);
+        }
+
+        if(dateValue === '') {
+            setErrorFor(date, 'Date cannot be blank');
+        } else {
+            setSuccessFor(date);
+        }
+    }
+    
+    function setErrorFor(input, message) {
+        const formControl = input.parentElement;
+        const small = formControl.querySelector('small');
+        formControl.className = 'form_container error';
+        small.innerText = message;
+    }
+    
+    function setSuccessFor(input) {
+        const formControl = input.parentElement;
+        formControl.className = 'form_container success';
+    }
+        
+    function isEmail(email) {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    }
+    
+
+    if(name.value == "" || num.value == "" || email.value == "")
+    {
+        alert("No Blank is allowed");
+        return false;        
+    }
+    else 
+    {
+        return true;
+    }
+}
+
+//date
+ var today = new Date().toISOString().split('T')[0];
+ document.getElementById("date-input").setAttribute("min",today);
+
+ /*
+ const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
+ */
